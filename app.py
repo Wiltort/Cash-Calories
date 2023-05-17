a1d6db7a3daecf9ae1466f9f27ff4dfb2472abe5
Виктор Королев <wiltort21@gmail.com>
2023-05-17 19:20:41 +0500

    Testing

--- app.py
+++ app.py
@@ -3,45 +3,65 @@ import datetime as dt
 date_format = '%d.%m.%Y'
 now = dt.datetime.now()
 td = now.date()
-print(now)
-print(td)
+
 class Record:
-    def __init__(self, amount, date, comment):
+    def __init__(self, amount, date='', comment):
         self.amount = amount
-        self.date = date
         self.comment = comment
+        if date == '':
+            self.date = td
+        else:
+            self.date = dt.datetime.strptime(date, date_format).date()
+        
 
 class Calculator:
     def __init__(self, limit):
         self.limit = limit
-        records = []
+        self.records = []
     def add_record(self, Record):
-        records.append(Record)
+        self.records.append(Record)
+    def get_today_stats(self):
+        tsumma = 0
+        for r in self.records:
+            if r.date == td:
+                tsumma += r.amount
+        return tsumma
+    def get_week_stats(self):
+        wsumma = 0
+        today = dt.date.today()
+        week_ago = today - dt.timedelta(days = 7)
+        for r in self.records:
+            if week_ago<=r.date<=today:
+                wsumma += r.amount
+        return wsumma
+
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        diff = self.limit - self.get_today_stats()
+        if self.get_today_stats() < self.limit:
+            print(f"Сегодня можно съесть что-нибудь ещё, но с общей
+                  калорийностью не более {diff} кКал")
+        else:
+            print("Хватит есть!")
+    
+
     
 class CashCalculator(Calculator):
-    def get_today_stats(self):
-        sum = 0
-        for r in records:
-            if r.date == now.date():
-                sum += r.amount
-        print(f"Сегодня, {now.date()}, потрачено {sum} руб")
+    USD_RATE = 80
+    EUR_RATE = 88
+    def get_today_cash_remained(self,currency):
+        diff = self.limit - self.get_today_stats()
+        if currency == rub:
+            rate = 1
+        elif currency = usd:
+            rate = USD_RATE
+        else:
+            rate = EURO_RATE
+        diff = diff/rate
+        if diff>0:
+            print(f"На сегодня осталось {diff} {currency}")
+        elif diff == 0:
+            print("Денег нет, держись")
+        else:
+            print("Денег нет, твой долг - {-diff} {currency}")
         
-
-
-
-
-
-
-
-# для CashCalculator 
-r1 = Record(amount=145, comment="Безудержный шопинг", date="08.03.2019")
-r2 = Record(amount=1568, comment="Наполнение потребительской корзины", date="09.03.2019")
-r3 = Record(amount=691, comment="Катание на такси", date="08.03.2019")
-
-# для CaloriesCalculator
-r4 = Record(amount=1186, comment="Кусок тортика. И ещё один.", date="24.02.2019")
-r5 = Record(amount=84, comment="Йогурт.", date="23.02.2019")
-r6 = Record(amount=1140, comment="Баночка чипсов.", date="24.02.2019")
-
-
-    
